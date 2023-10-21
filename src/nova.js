d6bf9e09f7aaa92e0baf42a5ac5970abac6edcb1
Jomo Kigo <kigojay684@gmail.com>
2023-10-21 13:46:14 +0300

    Changes to be committed: 	new file:   .vscode/settings.json 	new file:   res/fonts/ABeeZee/AbeezeeItalic.otf 	new file:   res/fonts/ABeeZee/AbeezeeRegular.otf 	new file:   res/fonts/ABeeZee/info.txt 	new file:   res/fonts/ABeeZee/misc/SIL Open Font License.txt 	new file:   res/fonts/Aesthetic/!!! Read Me!!!.pdf 	new file:   res/fonts/Aesthetic/Aesthetic Preview/2.jpg 	new file:   res/fonts/Aesthetic/Aesthetic Preview/3.jpg 	new file:   res/fonts/Aesthetic/Aesthetic Preview/4.jpg 	new file:   res/fonts/Aesthetic/Aesthetic Preview/5.jpg 	new file:   res/fonts/Aesthetic/Aesthetic Preview/6.jpg 	new file:   res/fonts/Aesthetic/Aesthetic Preview/7.jpg 	new file:   res/fonts/Aesthetic/Aesthetic Preview/cover.jpg 	new file:   res/fonts/Aesthetic/Aesthetic-Regular.otf 	new file:   res/fonts/Aesthetic/Aesthetic-Regular.ttf 	new file:   res/fonts/Aesthetic/Aesthetic-Regular.woff 	new file:   res/fonts/Aesthetic/Aesthetic-Regular.woff2 	new file:   res/fonts/Aesthetic/Dafont Cover - Aesthetic.png 	new file:   res/fonts/aloevera-font/Aloevera-OVoWO.ttf 	new file:   res/fonts/aloevera-font/info.txt 	new file:   res/fonts/aloevera-font/misc/Readme.txt 	new file:   res/fonts/cakadidi/Cakadidi.ttf 	new file:   res/fonts/install/Install.otf 	new file:   res/fonts/metalsmith-font/MetalsmithRegular-p7ylO.otf 	new file:   res/fonts/metalsmith-font/MetalsmithRegular-x3yMq.ttf 	new file:   res/fonts/metalsmith-font/info.txt 	new file:   res/fonts/metalsmith-font/misc/Visit Here for More Fonts and Freebies-2435.url 	new file:   res/fonts/metalsmith-font/misc/readme-223c.txt 	new file:   res/fonts/modern-age/License.txt 	new file:   res/fonts/modern-age/Modern Age Italic.otf 	new file:   res/fonts/modern-age/Modern Age.png 	new file:   res/fonts/modern-age/Modern_Age.otf 	new file:   res/fonts/modern-age/Modern_Age.ttf 	new file:   res/fonts/modern-age/Modern_Age_Italic.ttf 	new file:   res/fonts/progress-font/ProgressPersonalUse-EaJdz.ttf 	new file:   res/fonts/progress-font/info.txt 	new file:   res/fonts/progress-font/misc/Terms of Use End User Lisence Agreement.txt 	new file:   res/fonts/queensides-font/Queensides-3z7Ey.ttf 	new file:   res/fonts/queensides-font/QueensidesLight-ZVj3l.ttf 	new file:   res/fonts/queensides-font/QueensidesMedium-x30zV.ttf 	new file:   res/fonts/queensides-font/info.txt 	new file:   res/fonts/queensides-font/misc/README LICENSE!!!.txt 	new file:   res/images/Alley_She.jpg 	new file:   res/images/Avatar.png 	new file:   res/images/Avatar2.png 	new file:   res/images/Background Circles.png 	new file:   res/images/CEO.jpg 	new file:   res/images/Graphic.png 	new file:   res/images/Graphic_design.jpg 	new file:   res/images/HeadShot.png 	new file:   res/images/Illustrator.png 	new file:   res/images/Instagram.png 	new file:   res/images/LinkedIn.png 	new file:   res/images/Logo.png 	new file:   res/images/Pic.png 	new file:   res/images/Potrait_1.jpg 	new file:   res/images/Responsive.png 	new file:   res/images/Selfie_Shot.png 	new file:   res/images/Tape.jpg 	new file:   res/images/TikTok.png 	new file:   res/images/UI.png 	new file:   res/images/UI_UX.jpg 	new file:   res/images/User2_Dalle.png 	new file:   res/images/Video.png 	new file:   res/images/Web.png 	new file:   res/images/YouTube.png 	new file:   res/images/brand img.png 	new file:   res/images/handshake.jpg 	new file:   res/images/illustration.jpg 	new file:   res/images/img_backg.png 	new file:   res/images/user.jpg 	new file:   res/images/user2.png 	new file:   res/images/video_edit.jpg 	new file:   res/images/web_dev - Copy.jpg 	new file:   res/images/web_dev.jpg 	new file:   res/images/web_developer.jpg 	new file:   rough.css 	new file:   rough.html 	new file:   src/info.txt 	new file:   src/lorem.txt 	new file:   src/nova.html 	new file:   src/nova.js 	new file:   src/service.css 	new file:   src/service.css.map 	new file:   src/service.scss

---
+++ src/nova.js
@@ -0,0 +1,36 @@
+// nova.js
+
+// var scrollContainer = document.querySelector('.service-cont');
+// var scrollItems = document.querySelectorAll('.service-item');
+// var currentIndex = 0;
+// var scrollSpeed = 0.1;
+// var delay = 5000;
+
+// function autoScroll() {
+//   // Calculate the next index
+//   var nextIndex = currentIndex + 1;
+
+//   // Check if we have reached the last item
+//   if (nextIndex >= scrollItems.length) {
+//     nextIndex = 0;
+//   }
+
+//   // Get the scroll position of the next item
+//   var nextScrollPosition = scrollItems[nextIndex].offsetLeft - scrollContainer.offsetLeft;
+
+//   // Animate the scroll to the next item
+//   scrollContainer.scrollTo({
+//     left: nextScrollPosition,
+//     behavior: 'smooth'
+//   });
+
+//   // Update the current index
+//   currentIndex = nextIndex;
+
+//   // Call the autoScroll function repeatedly after the specified delay
+//   setTimeout(autoScroll, delay);
+// }
+
+// // Call the autoScroll function after the specified delay
+// setTimeout(autoScroll, delay);
+
